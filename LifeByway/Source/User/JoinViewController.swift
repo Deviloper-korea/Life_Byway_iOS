202a189eda27e6562204330522e52e564dcafb1a
Minseob Yoon <yoon.minseob@navercorp.com>
2018-04-05 02:52:28 +0900

    회원가입 텍스트필드 추가

--- LifeByway/Source/User/JoinViewController.swift
+++ LifeByway/Source/User/JoinViewController.swift
@@ -10,4 +10,42 @@ import UIKit
 
 class JoinViewController: BaseViewController {
     
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordCheckTextField: UITextField!
+}
+
+// @IBAction
+extension JoinViewController {
+    
+    @IBAction func registerProfileImageButtonTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func authButtonTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func joinButtonTapped(sender: UIButton) {
+        
+    }
+}
+
+// UITextFieldDelegate
+extension JoinViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case idTextField:
+            nicknameTextField.becomeFirstResponder()
+        case nicknameTextField:
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            passwordCheckTextField.becomeFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
+    }
 }
