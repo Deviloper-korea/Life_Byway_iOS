470b8b3d0890f5201a3f6fc7688f1e818263fbbd
Minseob Yoon <yoon.minseob@navercorp.com>
2018-04-04 15:06:00 +0900

    API Configuration 세팅

---
+++ LifeByway/Source/API/APIConfiguration.swift
@@ -0,0 +1,25 @@
+//
+//  APIConfiguration.swift
+//  LifeByway
+//
+//  Created by Minseob Yoon on 2018. 4. 2..
+//  Copyright © 2018년 Deviloper. All rights reserved.
+//
+
+import Foundation
+
+enum ServerEnvironment {
+    case dev
+    case real
+    
+    var domain: String {
+        switch self {
+        case .dev: return "http://13.125.236.45/"
+        case .real: return ""
+        }
+    }
+}
+
+struct APIConfiguration {
+    static let serverEnvironment: ServerEnvironment = .dev
+}
