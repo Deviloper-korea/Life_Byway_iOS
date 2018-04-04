86ee730c79192873598b0bd899633bf04c995ed2
hongyun0 <hongyun0@naver.com>
2018-04-04 15:04:49 +0900

    탭에 해당하는 기본 컨트롤러 추가

---
+++ LifeByway/Source/MyPage/MyPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MyPageViewController.swift
+//  LifeByway
+//
+//  Created by yunyoung on 2018. 3. 27..
+//  Copyright © 2018년 Deviloper. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: BaseViewController {
+    
+}
---
+++ LifeByway/Source/User/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  LifeByway
+//
+//  Created by yunyoung on 2018. 3. 27..
+//  Copyright © 2018년 Deviloper. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+    
+}
