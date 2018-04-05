1e23c317d7ebc49938530c9444d7f9e500f882a4
Yunyoung Hong <hongyun0@naver.com>
2018-04-05 23:16:29 +0900

    login interface 구현, outlet 연결

--- LifeByway/Source/User/LoginViewController.swift
+++ LifeByway/Source/User/LoginViewController.swift
@@ -8,6 +8,34 @@
 
 import UIKit
 
-class LoginViewController: BaseViewController {
+class LoginViewController: BaseViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var findUserInfoButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == idTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        //id, pw value gogo
+    }
+    
+    @IBAction func joinButtonTapped(_ sender: UIButton) {
+        //go to joinscreen
+    }
+    
+    @IBAction func findButtonTapped(_ sender: UIButton) {
+        //go to findUserInfoscreen
+    }
     
 }
